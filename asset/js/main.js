65ef8eb125804298b19523d9548be05cbaf36847
Simone Saputo <simonesaputo0@gmail.com>
2022-03-16 11:46:50 +0100

    adj-1

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,5 +6,5 @@ let numero_2 = prompt('Inserisci il secondo numero');
 if(numero_1 > numero_2) {
     document.getElementById('box').innerHTML = `Il numero 1 è il maggiore: ${numero_1}`;
 } else {
-    document.getElementById('box').innerHTML = `Il numero 2 è il maggiore: ${numero_2}`;
+    document.getElementById('box_2').innerHTML = `Il numero 2 è il maggiore: ${numero_2}`;
 }
