33b7a0863c8e67b812211c22f9e62b46d7560d02
Simone Saputo <simonesaputo0@gmail.com>
2022-03-16 12:25:19 +0100

    Snack-2

--- Snack-1/asset/js/main.js
+++ Snack-1/asset/js/main.js
@@ -5,6 +5,8 @@ let numero_2 = prompt('Inserisci il secondo numero');
 
 if(numero_1 > numero_2) {
     document.getElementById('box').innerHTML = `Il numero 1 è il maggiore: ${numero_1}`;
-} else {
+} else if (numero_1 < numero_2){
     document.getElementById('box_2').innerHTML = `Il numero 2 è il maggiore: ${numero_2}`;
+} else {
+    document.getElementById('box_3').innerHTML = `Pari, Inserisci nuovi numeri: `;
 }
---
+++ Snack-2/asset/js/main.js
@@ -0,0 +1,10 @@
+// L'utente inserisce due parole in successione con due prompt, il software stampa prima la parola più corta, poi la parola più lunga
+
+let parola_1 = prompt('Inserisci la prima parola');
+let parola_2 = prompt('Inserisci la seconda parola');
+
+if(parola_1.length < parola_2.length) {
+    document.getElementById('box').innerHTML = `La parola 1 è la più corta: ${parola_1}. Mentre la parola 2 è la più lunga: ${parola_2}`;
+} else {
+    document.getElementById('box_2').innerHTML = `La parola 2 è la più corta: ${parola_2}. Mentre la parola 1 è la più lunga: ${parola_1}`;
+}
