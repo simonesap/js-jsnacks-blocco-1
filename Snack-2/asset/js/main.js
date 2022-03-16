b92798276bf9bf0c632508ccc96a961fa929f389
Simone Saputo <simonesaputo0@gmail.com>
2022-03-16 12:30:14 +0100

    adj-2

--- Snack-2/asset/js/main.js
+++ Snack-2/asset/js/main.js
@@ -5,6 +5,8 @@ let parola_2 = prompt('Inserisci la seconda parola');
 
 if(parola_1.length < parola_2.length) {
     document.getElementById('box').innerHTML = `La parola 1 è la più corta: ${parola_1}. Mentre la parola 2 è la più lunga: ${parola_2}`;
-} else {
+} else if (parola_2.length < parola_1.length) {
     document.getElementById('box_2').innerHTML = `La parola 2 è la più corta: ${parola_2}. Mentre la parola 1 è la più lunga: ${parola_1}`;
+} else {
+    document.getElementById('box_3').innerHTML = `La lunghezza delle parole è identica, inserisci nuove parole: `;
 }
