52ec1de18dacce358f244260a0bef1fff2283937
Simone Saputo <simonesaputo0@gmail.com>
2022-03-16 11:43:59 +0100

    snack-1

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+// L'utente inserisce due numeri in successione con due prompt, il software stampa il maggiore
+
+let numero_1 = prompt('Inserisci il primo numero');
+let numero_2 = prompt('Inserisci il secondo numero');
+
+if(numero_1 > numero_2) {
+    document.getElementById('box').innerHTML = `Il numero 1 è il maggiore: ${numero_1}`;
+} else {
+    document.getElementById('box').innerHTML = `Il numero 2 è il maggiore: ${numero_2}`;
+}
